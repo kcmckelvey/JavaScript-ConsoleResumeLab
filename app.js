34d4241f1d6bdf291032f6dcfad3527bf18420aa
Kevin McKelvey <kevinmckelvey920@gmail.com>
2018-08-21 17:18:12 -0500

    updates

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Name: Kevin McKelvey');
+console.log('Career: Hopefully a developer one day');
+console.log('Description: I like turtles');
+
+console.log('My Interests:');
+console.log('sports');
+console.log('music');
+console.log('movies');
+
+console.log('My Previous Experience:');
+console.log()
